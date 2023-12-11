f8bbd004aadbc1987c7ce29c6692a3c27517d38c
rahulpro1012 <rahulmudaliar1012@gmail.com>
2023-12-12 01:15:13 +0530

    second

--- Portfolio/src/components/Header.jsx
+++ Portfolio/src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
             <h1 className=' ml-4 text-6xl py-3 font-serif font-semibold text-yellow-500'>Hey I'm </h1>
             <h1 className='text-left ml-72 text-5xl font-serif font-semibold text-yellow-500 '>Rahul Mudaliar</h1>
         </div>
-        <p className='py-7 font-serif text-yellow-500 text-center ml-60 font-serif'> A passionate and dedicated Computer Science student, my journey revolves around mastering the art of fullstack development, seamlessly navigating both backend and frontend technologies. Driven by a fervor for problem-solving, I am unwavering in my commitment to perpetual learning, always seeking novel challenges in the dynamic realm of software development</p>
+        <p className='py-7  text-yellow-500 text-center ml-60 font-serif'> A passionate and dedicated Computer Science student, my journey revolves around mastering the art of fullstack development, seamlessly navigating both backend and frontend technologies. Driven by a fervor for problem-solving, I am unwavering in my commitment to perpetual learning, always seeking novel challenges in the dynamic realm of software development</p>
     </div>
   )
 }
