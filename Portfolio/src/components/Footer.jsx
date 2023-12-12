3a4ad2dd7348a054fea3b555455bff7540dbea03
rahulpro1012 <rahulmudaliar1012@gmail.com>
2023-12-13 02:40:03 +0530

    layout changes

--- Portfolio/src/components/Footer.jsx
+++ Portfolio/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='border-x-8 border-green-600 border-y-8'>
+    <div className='border-x-8 border-green-600 border-y-8 h-full'>
         <p className='text-yellow-500 text-2xl font-serif '>More about me....</p>
 
     </div>
--- Portfolio/src/components/Navbar.jsx
+++ Portfolio/src/components/Navbar.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='flex height-30 border-x-8 border-red-500 border-y-8'>
-          <ul className='flex-col justify-between  '>
-            <li className='text-3xl text-yellow-500'>Home</li>
-            <li className='text-3xl text-yellow-500'>Contact Me</li>
-            <li className='text-3xl text-yellow-500'>Projects</li>
-          </ul>
+    <div className='flex-col height-30 border-x-8 border-red-500 border-y-8 w-full justify-stretch'>
+          
+            <button className="bg-yellow-500 hover:bg-yellow-600 text-blue-950 font-serif py-2 px-4 font-semibold rounded-full shadow-md transition duration-300 ease-in-out transform hover:scale-105">
+            Home
+            </button>
+            <button className="bg-yellow-500 hover:bg-yellow-600 text-blue-950 font-serif py-2 px-4 font-semibold rounded-full shadow-md transition duration-300 ease-in-out transform hover:scale-105">
+            Contact Me-
+            </button>
+            <button className="bg-yellow-500 hover:bg-yellow-600 text-blue-950 font-serif py-2 px-4 font-semibold rounded-full shadow-md transition duration-300 ease-in-out transform hover:scale-105">
+            Projects
+            </button>
     </div>
   )
 }
